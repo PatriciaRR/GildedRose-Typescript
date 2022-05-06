1ffddbb544b4a58ebed15b4f290b9828afa02827
Patricia Robles <patriciarobles9@gmail.com>
2022-05-06 17:51:08 +0100

    standard 'items' isolated for quality and sellIn

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -23,6 +23,7 @@ export class GildedRose {
             
             if(this.items[i].name.includes('item') == true) {
                 //if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                    this.items[i].sellIn = this.items[i].sellIn - 1;
                 if (this.items[i].quality > 0 ) {
                     this.items[i].quality = this.items[i].quality - 1
                 }
@@ -32,9 +33,7 @@ export class GildedRose {
                 if (this.items[i].quality < 50) {
                     this.items[i].quality = this.items[i].quality + 1
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn < 11) {
-                        //if (this.items[i].sellIn < 11) {
                                 this.items[i].quality = this.items[i].quality + 1 
-                        //}
                         if (this.items[i].sellIn < 6) {
                                 this.items[i].quality = this.items[i].quality + 1
                         }
@@ -42,7 +41,7 @@ export class GildedRose {
                 }
             }
 
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' && this.items[i].name.includes('item') == false) {
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
             if (this.items[i].sellIn < 0) {
