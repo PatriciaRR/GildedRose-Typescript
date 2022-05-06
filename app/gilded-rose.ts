552759cf153ca6d1d6ab651248fec9952b122f27
Patricia Robles <patriciarobles9@gmail.com>
2022-05-06 22:19:06 +0100

    quality fixed for regular items

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -21,26 +21,33 @@ export class GildedRose {
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             
-            if(this.items[i].name.includes('item') == true) {
-                //if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            if (this.items[i].name.includes('item') == true) {
                     this.items[i].sellIn = this.items[i].sellIn - 1;
                 if (this.items[i].quality > 0 ) {
                     this.items[i].quality = this.items[i].quality - 1
+                    //continue;
                 }
             
-                
+            if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+                this.items[i].sellIn = this.items[i].sellIn - 1;
+                if (this.items[i].sellIn > 10) {
+                    this.items[i].quality = this.items[i].quality + 1;
+                }
+                if (this.items[i].sellIn < 11) {
+                    this.items[i].quality = this.items[i].quality + 1;
+                }
+                if (this.items[i].sellIn < 6) {
+                    this.items[i].quality = this.items[i].quality + 1;
+                }
+            } 
+
+            // if (this.items[i].name == 'Aged Brie')   
             // if(this.item[i].name == 'Sulfuras, Hand of Ragnaros') {
-                //this.items[i].sellIn = this.items[i].sellIn - 1
+                //this.items[i].quality = this.items[i].sellIn 
             //}
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert' && this.items[i].sellIn < 11) {
-                                this.items[i].quality = this.items[i].quality + 1 
-                        if (this.items[i].sellIn < 6) {
-                                this.items[i].quality = this.items[i].quality + 1
-                        }
-                    }
+
+            if (this.items[i].quality < 50 && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') { // these 2 lines need to go
+                    this.items[i].quality = this.items[i].quality + 0
                 }
             }
 
